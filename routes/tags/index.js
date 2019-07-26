271a8df1bdfd0914718d289db0d1691128768a74
Mohamed ayoub Zahir <mzahir@e1r5p12.1337.ma>
2019-07-26 11:02:04 +0100

    get tags

--- routes/tags/index.js
+++ routes/tags/index.js
@@ -1,7 +1,20 @@
 const	tagsRouter =  require('express').Router();
 const   query = require('../../libraries/database');
 
-tagsRouter.get('/', (req, res) => {
+tagsRouter.get('/get', (req, res) => {
+	query.run('MATCH (n:Tag) RETURN n;')
+		.then((results) => {
+			res.json(results);
+		});
+});
+
+tagsRouter.get('/get/:name', (req, res) => {
+	console.log(req.params);
+	query.run('MATCH (n:Tag { name: $name }) RETURN n', {name: req.params.name})
+		.then((results) => {
+			res.json(results);
+		})
+		.catch ((err) => console.log(err));
 });
 
 module.exports = tagsRouter;
