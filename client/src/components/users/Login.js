7349cdd8166b2465baaa7daa4b447f0f4554afda
Chakour Marouane <cmarouan@e1r6p11.1337.ma>
2019-07-28 18:39:20 +0100

    simple modify in client

--- client/src/components/users/Login.js
+++ client/src/components/users/Login.js
@@ -6,13 +6,13 @@ class Login extends Component {
     return (
     <div className="container">
         <form id="Login">
-            <div class="form-group">
-                <input type="email" class="form-control" id="inputEmail" placeholder="Email Address" />
+            <div className="form-group">
+                <input type="email" className="form-control" id="inputEmail" placeholder="Email Address" />
             </div>
-            <div class="form-group">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password" />
+            <div className="form-group">
+                <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
             </div>
-            <button type="submit" class="btn btn-danger">Login</button>
+            <button type="submit" className="btn btn-danger">Login</button>
         </form>
     </div>
     );
