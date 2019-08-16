c74b940fbf4b3731873d2e35ad652fd9db0a84f4
cmarouan <merouan.chakour@gmail.com>
2019-08-16 12:26:38 +0100

    handle error tags in front

--- client/src/components/users/editProfileCom/EditInfos.js
+++ client/src/components/users/editProfileCom/EditInfos.js
@@ -1,29 +1,21 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import Chip from "@material-ui/core/Chip";
 import CreatableSelect from "react-select/creatable";
 import RegisterInput from "../RegisterInput";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import axios from "axios";
-import {
-  getter
-} from "../../../helpers/tokenOperation";
-import {
-  getAllTags
-} from "../../../helpers/getAllTags";
-import {
-  getUserTags
-} from "../../../helpers/getUserTags";
-import {
-  addTags
-} from "../../../helpers/addTags";
+import { getter } from "../../../helpers/tokenOperation";
+import { getAllTags } from "../../../helpers/getAllTags";
+import { getUserTags } from "../../../helpers/getUserTags";
+import { addTags } from "../../../helpers/addTags";
 const head = {
   "auth-token": getter("token"),
   Accept: "application/json",
   "Content-Type": "application/json"
 };
-
+const tagsErrorStyle = {
+  color: 'red'
+};
 export default class EditInfos extends Component {
   state = {
     fName: "",
@@ -39,6 +31,7 @@ export default class EditInfos extends Component {
     errState: {},
     options: [],
     optionsUser: [],
+    tagsExit: "",
     visible: false
   };
   onChange = e => {
@@ -46,7 +39,8 @@ export default class EditInfos extends Component {
       [e.target.name]: e.target.value
     });
   };
-  onChangeTags = async (newValue) => {
+  onChangeTags = async newValue => {
+    this.setState({tagsExit: ''});
     if (newValue) {
       var Tags = {
         tagName: newValue.value
@@ -59,15 +53,19 @@ export default class EditInfos extends Component {
       this.setState({
         options: neww
       });
-      addTags(this.state.username, Tags, head).then(({
-        data
-      }) => {
-        this.calluserTags();
-        this.callTags();
-      }).catch(err => console.log('layn3altabonmok!'));
+      addTags(this.state.username, Tags, head)
+        .then(({ data }) => {
+          this.calluserTags();
+          this.callTags();
+        })
+        .catch(err => {
+          this.setState({tagsExit: 'error'});
+          this.setState({options: []});
+          });
     }
   };
   clearErrorState = () => {
+    this.setState({tagsExit: ''});
     this.setState({
       errState: {}
     });
@@ -76,9 +74,7 @@ export default class EditInfos extends Component {
     });
   };
   callTags = async () => {
-    await getAllTags().then(({
-      data
-    }) => {
+    await getAllTags().then(({ data }) => {
       this.setState({
         options: data.data.map(tag => {
           return {
@@ -90,16 +86,16 @@ export default class EditInfos extends Component {
       let yFilter = this.state.optionsUser.map(itemY => {
         return itemY.value;
       });
-      let filteredTags = this.state.options.filter(itemX => !yFilter.includes(itemX.value));
+      let filteredTags = this.state.options.filter(
+        itemX => !yFilter.includes(itemX.value)
+      );
       this.setState({
         options: filteredTags
       });
     });
-  }
+  };
   calluserTags = () => {
-    getUserTags(this.props.username).then(({
-      data
-    }) => {
+    getUserTags(this.props.username).then(({ data }) => {
       this.setState({
         optionsUser: data.data.map(tag => {
           return {
@@ -109,15 +105,13 @@ export default class EditInfos extends Component {
         })
       });
     });
-
-  }
+  };
   ondeleteTag = () => {
     console.log(this.state.optionsUser);
     console.log("tag will be deleted");
-  }
+  };
 
   async componentWillMount() {
-
     const user = this.props.username;
     this.calluserTags(this.state.user);
     this.setState({
@@ -126,27 +120,34 @@ export default class EditInfos extends Component {
     await axios.get(`http://localhost:1337/api/users/get/${user}`).then(res => {
       if (res.data.data.props) {
         const user = res.data.data.props;
-        if (user.fName) this.setState({
-          fName: user.fName
-        });
-        if (user.lName) this.setState({
-          lName: user.lName
-        });
-        if (user.gender) this.setState({
-          gender: user.gender
-        });
-        if (user.location) this.setState({
-          location: user.location
-        });
-        if (user.bio) this.setState({
-          bio: user.bio
-        });
-        if (user.sexualPref) this.setState({
-          sexualPref: user.sexualPref
-        });
-        if (user.birthDate) this.setState({
-          birthDate: user.birthDate
-        });
+        if (user.fName)
+          this.setState({
+            fName: user.fName
+          });
+        if (user.lName)
+          this.setState({
+            lName: user.lName
+          });
+        if (user.gender)
+          this.setState({
+            gender: user.gender
+          });
+        if (user.location)
+          this.setState({
+            location: user.location
+          });
+        if (user.bio)
+          this.setState({
+            bio: user.bio
+          });
+        if (user.sexualPref)
+          this.setState({
+            sexualPref: user.sexualPref
+          });
+        if (user.birthDate)
+          this.setState({
+            birthDate: user.birthDate
+          });
         this.setState({
           visible: true
         });
@@ -195,274 +196,193 @@ export default class EditInfos extends Component {
       });
   };
   render() {
-    return ( <
-      div >
-      <
-      div className = "profile-content" >
-      <
-      form onSubmit = {
-        this.editInfo
-      } >
-      <
-      div className = "row" >
-      <
-      div className = "col" > {
-        this.state.visible !== "" && ( <
-          RegisterInput label = "First name"
-          type = "text"
-          name = "fName"
-          id = "fName"
-          value = {
-            this.state.fName
-          }
-          placeholder = "Enter first name"
-          err = {
-            this.state.errState.fName
-          }
-          onChange = {
-            this.onChange
-          }
-          />
-        )
-      } <
-      /div> <
-      div className = "col" > {
-        this.state.visible !== "" && ( <
-          RegisterInput label = "Last name"
-          type = "text"
-          name = "lName"
-          id = "lName"
-          value = {
-            this.state.lName
-          }
-          placeholder = "Enter last name"
-          err = {
-            this.state.errState.lName
-          }
-          onChange = {
-            this.onChange
-          }
-          />
-        )
-      } <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      label > Gender < /label> <
-      select id = "gender"
-      defaultValue = {
-        this.state.gender
-      }
-      onChange = {
-        this.handleGender
-      }
-      className = "form-control" >
-      <
-      option value = "Male" > Male < /option> <
-      option value = "Female" > Female < /option> <
-      option value = "Other" > Other < /option> <
-      /select> <
-      /div> <
-      div className = "col" > {
-        this.state.visible !== "" && ( <
-          RegisterInput label = "sexualPref"
-          type = "text"
-          name = "sexualPref"
-          id = "sexualPref"
-          value = {
-            this.state.sexualPref
-          }
-          placeholder = "Enter your sexualPref"
-          err = {
-            this.state.errState.sexualPref
-          }
-          onChange = {
-            this.onChange
-          }
-          />
-        )
-      } <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" > {
-        this.state.visible !== "" && ( <
-          RegisterInput label = "Location"
-          type = "text"
-          name = "location"
-          id = "location"
-          value = {
-            this.state.location
-          }
-          placeholder = "Enter your location"
-          err = {
-            this.state.errState.location
-          }
-          onChange = {
-            this.onChange
-          }
-          />
-        )
-      } <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      label >
-      <
-      small > Your birthDay < /small> {this.state.birthDate} <
-      /label> <
-      br / > {
-        this.state.visible !== "" && ( <
-          TextField id = "date"
-          type = "date"
-          onChange = {
-            this.handleBirthday
-          }
-          defaultValue = {
-            this.state.birthDate
-          }
-          InputLabelProps = {
-            {
-              shrink: true
-            }
-          }
-          />
-        )
-      } <
-      /div> <
-      div className = "col" >
-      <
-      label > Active location < /label> <
-      select onChange = {
-        this.handleActiveNotif
-      }
-      id = "activeNotf"
-      defaultValue = {
-        ""
-      }
-      className = "form-control" >
-      <
-      option value = "1" > True < /option> <
-      option value = "0" > False < /option> <
-      /select> <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      label > Biography < /label> {
-        this.state.visible !== "" && ( <
-          textarea className = "form-control"
-          id = "bio"
-          rows = "3"
-          name = "bio"
-          placeholder = "Bio"
-          value = {
-            this.state.bio
-          }
-          onChange = {
-            this.onChange
-          }
-          />
-        )
-      } <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      label > Add tags < /label> <
-      CreatableSelect isClearable onChange = {
-        this.onChangeTags
-      }
-      //onInputChange={this.handleInputChange}
-      options = {
-        this.state.options
-      }
-      /> <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      label > My tags < /label> <
-      br / > {
-        this.state.optionsUser.map(tag => {
-          return ( <
-            Chip key = {
-              tag.label
-            }
-            label = {
-              tag.label
-            }
-            value = {
-              tag.value
-            }
-            onDelete = {
-              this.ondeleteTag
-            }
-            onChange = {
-              this.handleChangeTag
-            }
-            className = ""
-            color = "primary" /
-            >
-          );
-        })
-      } <
-      /div> <
-      /div> <
-      br / >
-      <
-      div className = "row" >
-      <
-      div className = "col" >
-      <
-      button type = "submit"
-      className = "btn btn-primary" >
-      Save <
-      /button> <
-      /div> <
-      div className = "col" > {
-        this.state.msg1 && ( <
-          div className = "alert alert-primary"
-          role = "alert" > {
-            " "
-          } {
-            this.state.msg1
-          } {
-            " "
-          } <
-          /div>
-        )
-      } <
-      /div> <
-      /div> <
-      /form> <
-      /div> <
-      /div>
+    return (
+      <div>
+        <div className="profile-content">
+          <form onSubmit={this.editInfo}>
+            <div className="row">
+              <div className="col">
+                {" "}
+                {this.state.visible !== "" && (
+                  <RegisterInput
+                    label="First name"
+                    type="text"
+                    name="fName"
+                    id="fName"
+                    value={this.state.fName}
+                    placeholder="Enter first name"
+                    err={this.state.errState.fName}
+                    onChange={this.onChange}
+                  />
+                )}{" "}
+              </div>{" "}
+              <div className="col">
+                {" "}
+                {this.state.visible !== "" && (
+                  <RegisterInput
+                    label="Last name"
+                    type="text"
+                    name="lName"
+                    id="lName"
+                    value={this.state.lName}
+                    placeholder="Enter last name"
+                    err={this.state.errState.lName}
+                    onChange={this.onChange}
+                  />
+                )}{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <label> Gender </label>{" "}
+                <select
+                  id="gender"
+                  defaultValue={this.state.gender}
+                  onChange={this.handleGender}
+                  className="form-control"
+                >
+                  <option value="Male"> Male </option>{" "}
+                  <option value="Female"> Female </option>{" "}
+                  <option value="Other"> Other </option>{" "}
+                </select>{" "}
+              </div>{" "}
+              <div className="col">
+                {" "}
+                {this.state.visible !== "" && (
+                  <RegisterInput
+                    label="sexualPref"
+                    type="text"
+                    name="sexualPref"
+                    id="sexualPref"
+                    value={this.state.sexualPref}
+                    placeholder="Enter your sexualPref"
+                    err={this.state.errState.sexualPref}
+                    onChange={this.onChange}
+                  />
+                )}{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                {" "}
+                {this.state.visible !== "" && (
+                  <RegisterInput
+                    label="Location"
+                    type="text"
+                    name="location"
+                    id="location"
+                    value={this.state.location}
+                    placeholder="Enter your location"
+                    err={this.state.errState.location}
+                    onChange={this.onChange}
+                  />
+                )}{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <label>
+                  <small> Your birthDay </small> {this.state.birthDate}{" "}
+                </label>{" "}
+                <br />{" "}
+                {this.state.visible !== "" && (
+                  <TextField
+                    id="date"
+                    type="date"
+                    onChange={this.handleBirthday}
+                    defaultValue={this.state.birthDate}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                  />
+                )}{" "}
+              </div>{" "}
+              <div className="col">
+                <label> Active location </label>{" "}
+                <select
+                  onChange={this.handleActiveNotif}
+                  id="activeNotf"
+                  defaultValue={""}
+                  className="form-control"
+                >
+                  <option value="1"> True </option>{" "}
+                  <option value="0"> False </option>{" "}
+                </select>{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <label> Biography </label>{" "}
+                {this.state.visible !== "" && (
+                  <textarea
+                    className="form-control"
+                    id="bio"
+                    rows="3"
+                    name="bio"
+                    placeholder="Bio"
+                    value={this.state.bio}
+                    onChange={this.onChange}
+                  />
+                )}{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <label> Add tags </label>{" "}
+                <CreatableSelect
+                  isClearable
+                  onChange={this.onChangeTags}
+                  //onInputChange={this.handleInputChange}
+                  options={this.state.options}
+                />{" "}
+                {this.state.tagsExit && 
+                <small style={tagsErrorStyle}>Tags aleardy exist</small>}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <label > My tags </label> <br />{" "}
+                {this.state.optionsUser.map(tag => {
+                  return (
+                    <Chip
+                      key={tag.label}
+                      label={tag.label}
+                      value={tag.value}
+                      onDelete={this.ondeleteTag}
+                      onChange={this.handleChangeTag}
+                      className=""
+                      color="primary"
+                    />
+                  );
+                })}{" "}
+              </div>{" "}
+            </div>{" "}
+            <br />
+            <div className="row">
+              <div className="col">
+                <button type="submit" className="btn btn-primary">
+                  Save{" "}
+                </button>{" "}
+              </div>{" "}
+              <div className="col">
+                {" "}
+                {this.state.msg1 && (
+                  <div className="alert alert-primary" role="alert">
+                    {" "}
+                    {this.state.msg1}{" "}
+                  </div>
+                )}{" "}
+              </div>{" "}
+            </div>{" "}
+          </form>{" "}
+        </div>{" "}
+      </div>
     );
   }
 }
